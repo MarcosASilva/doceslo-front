a08c38fb5ddab05788f1a8b4a36d0b9bbb8e889b
Marcos Assis <marcosvlk15@gmail.com>
2020-02-11 17:00:48 -0300

    first commit

---
+++ src/components/Bolo.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+
+export default class Bolo extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="itens">
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card  cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card  cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card  cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card  cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className=" item ">
+            <div className="card card-conteudo">
+              <img
+                className="card-img-top"
+                src="https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
+                alt="Card  cap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Bolos</h5>
+                <p className="card-text">Conheça</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
